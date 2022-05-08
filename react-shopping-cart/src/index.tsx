9f485d3a42dddea3de288419f5be2785d12a9cad
Kaushik Chemburkar <attherate@gmail.com>
2022-05-08 21:56:28 +1000

    Have a few errors on the console log - isLoading and error are putting out errors

--- react-shopping-cart/src/index.tsx
+++ react-shopping-cart/src/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 const client = new QueryClient();
 
-ReactDOM.render(
+const container = document.getElementById("root") as HTMLElement;
+const rootContainer = ReactDOM.createRoot(container);
+rootContainer.render(
   <QueryClientProvider client={client}>
     <App />
-  </QueryClientProvider>,
-  document.getElementById("root")
+  </QueryClientProvider>
 );
